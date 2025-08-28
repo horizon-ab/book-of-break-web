6948fd6ecc20e53c226088b4c26a5d298fad687f
Bailey Say <baileyasay@gmail.com>
2025-08-28 00:49:25 -0700

    init firebase servers

---
+++ src/lib/serverApp.ts
@@ -0,0 +1,21 @@
+import "server-only"
+
+import { cookies } from "next/headers";
+import { initializeServerApp, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+export async function getAuthenticatedAppForUser() {
+    const authIdToken = (await cookies()).get("__session")?.value;
+
+    const firebaseServerApp = initializeServerApp(
+        initializeApp(),
+        {
+            authIdToken
+        }
+    );
+
+    const auth = getAuth(firebaseServerApp);
+    await auth.authStateReady();
+
+    return { firebaseServerApp, currentUser: auth.currentUser };
+}

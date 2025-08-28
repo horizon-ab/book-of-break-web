ae4cd445e1310012855d40f150646756f1121466
Bailey Say <baileyasay@gmail.com>
2025-08-28 14:16:07 -0700

    can sign in with google

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+
+import { signInWithGoogle, signOut } from "@/lib/auth"
+
+export default function LoginPage() {
+
+    const handleSignIn = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        signInWithGoogle();
+    }
+
+    const handleSignOut = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        signOut();
+    }
+
+    return (
+        <div className="flex flex-col gap-10">
+            <Button onClick={handleSignIn}>Log In</Button>
+            <Button onClick={handleSignOut}>Log Out</Button>
+        </div>
+    )
+}
